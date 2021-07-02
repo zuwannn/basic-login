31ae663d30cb65cc78b8ac0efce270581923d662
zuwannn <suwanneethongtha@gmail.com>
2021-07-02 15:30:42 +0700

    Add files via upload

---
+++ server.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "register_db";
+
+// create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// check connection
+if (!$conn) {
+    die("Connection Failed" . mysqli_connect_error());
+}
+?>
